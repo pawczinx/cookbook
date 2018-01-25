a357659d58e73538589b28396cc5a023f9bc7aa5
Pawel <pawczinx@gmail.com>
2018-01-25 16:54:45 +0100

    abc

--- src/main/java/com/pawcio/account/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pawcio.account.repository;
-
-import com.pawcio.account.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long>{
-}
--- src/main/java/com/pawcio/account/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.pawcio.account.repository;
-
-import com.pawcio.account.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
